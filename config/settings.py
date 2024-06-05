f992fb36a7652bc819c4260e76eebbca56c6b110
sumika-t <sm-takagi@unext-hd.com>
2024-06-05 13:28:00 +0900

    make migrations

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+ 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'sample@internetacademy.co.jp'
+
