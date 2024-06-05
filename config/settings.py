af154519a1dec7d6791c055e75a18e9017d2c8ba
sumika-t <sm-takagi@unext-hd.com>
2024-06-05 14:20:23 +0900

    maek app contacs

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts.apps.ContactsConfig',
     'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
